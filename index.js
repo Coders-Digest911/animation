6b6166f5814efe90e20c667dec8fcfb53833d526
Coders-Digest911 <78774961+Coders-Digest911@users.noreply.github.com>
2021-04-11 15:13:28 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+   anime({
+            targets:".bottomBar",
+            width: 100 + "%",
+            direction: "alternate",
+            loop: true,
+            easing:"easeInOutSine"
+        })
+        anime({
+            targets:".rightBar",
+            height: 100 + "%",
+            top: 0,
+            direction: "alternate",
+            loop: true,
+            easing:"easeInOutSine"
+        })
+        anime({
+            targets:".leftBar",
+            height: 100 + "%",
+            direction: "alternate",
+            loop: true,
+            easing:"easeInOutSine"
+        })
+        anime({
+            targets:".topBar",
+            width: 0 + "%",
+            direction: "alternate",
+            loop: true,
+            easing:"easeInOutSine"
+        })
+        anime({
+            targets: '.backgroundGrid',
+          scale: [
+            {value: .1, easing: 'easeOutSine', duration: 500},
+            {value: 1, easing: 'easeInOutQuad', duration: 1200}
+          ],
+          delay: anime.stagger(200, {grid: [14, 5], from: 'center'}),
+          loop:true,
+});
