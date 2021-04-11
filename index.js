b477cd11e602f7bbc4860f0b4d63fd1a7650b905
Coders-Digest911 <78774961+Coders-Digest911@users.noreply.github.com>
2021-04-11 15:28:35 -0700

    Update index.js

--- index.js
+++ index.js
@@ -27,12 +27,14 @@
             loop: true,
             easing:"easeInOutSine"
         })
-        anime({
+      anime({
             targets: '.backgroundGrid',
           scale: [
             {value: .1, easing: 'easeOutSine', duration: 500},
             {value: 1, easing: 'easeInOutQuad', duration: 1200}
           ],
+          direction: "alternate",
+          backgroundColor: "rgb(166, 157, 207)",
           delay: anime.stagger(200, {grid: [14, 5], from: 'center'}),
-          loop:true,
+          loop:true
 });
